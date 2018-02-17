4c8fdd9caa63c247914d95bc27599b962b725281
mp3jjk <jhjung@lanada.kaist.ac.kr>
2018-02-18 02:40:55 +0900

    script ing

--- lanada_orchestra/sim_result/run.sh
+++ lanada_orchestra/sim_result/run.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 
-SR=0 # Decide whether SR simulation runs or not
-LR=1 # For LR case
-ONLY_LONG=0 # SR = 1 with only Long
-TRAFFIC=0 # 0 = periodic, 1 = poisson
-VAR_PERIOD=(5 15 30 60)
-VAR_ARRIVAL=(10)
-VAR_TOPOLOGY=("36grid_mrm2")
-VAR_LR_RANGE=("2X")
-VAR_LR_WEIGHT=(1 2 3 4 5)
-VAR_LSA_R=0
-VAR_STROBE_CNT=0
-VAR_ALPHA=(1)
-VAR_ALPHA_DIV=(1)
-VAR_PARENT_REDUCTION=0
-VAR_REDUCTION_RATIO=0
-VAR_DATA_ACK=1
+TSCH=0 # Whether Async(0) or TSCH(1)
+ORCHESTRA=0 # Whether Minimal(0) or Orchestra(1)
+RBS_SBS=0 # Whether RBS(0) or SBS(1)
+TRAFFIC=0 # Whether Periodic(0) or Poisson(1)
+ADAPTIVE_MDOE=0 # Whether basic(0) or adaptive(1)
+VAR_PERIOD=(5)
+VAR_ARRIVAL=(1)
+VAR_TOPOLOGY=("grid_36") # tree_c2_31 tree_c3_40 grid_36 random_50
+LABEL="test"
+SEED_NUMBER=("1")
+
 VAR_CHECK_RATE=(8)
-VAR_LSA_ENHANCED=0
-VAR_ROUTING_NO_ENERGY=0
-DATE="ltmax"
-LSA_MAC=1
-SEED_NUMBER=("1" "2" "3" "4" "5")
-MRM=1
-VAR_PERCENT=("1")
 
-# SR_RANGE simulation
+# Async sim
 
-if [ $SR -eq 1 ]
+if [ $TSCH -eq 0 ]
 then
     if [ $TRAFFIC -eq 0 ]
     then
@@ -37,21 +25,9 @@ then
 	    do
 		for topology in "${VAR_TOPOLOGY[@]}"
 		do
-		    for range in "${VAR_LR_RANGE[@]}"
+		    for check in "${VAR_CHECK_RATE[@]}"
 		    do
-			for alpha in "${VAR_ALPHA[@]}"
-			do
-			    for alpha_div in "${VAR_ALPHA_DIV[@]}"
-			    do
-				for percent in "${VAR_PERCENT[@]}"
-				do
-				    for check in "${VAR_CHECK_RATE[@]}"
-				    do
-				    ./sr_run.sh $topology $TRAFFIC $period  0 $alpha $VAR_STROBE_CNT "${DATE}" $VAR_DATA_ACK $alpha_div 0 $range $check $VAR_ROUTING_NO_ENERGY $seed $MRM $percent
-				    done
-				done
-			    done
-			done
+			./async_run.sh $topology $TRAFFIC $period 0 "${LABEL}" $check $seed $TSCH
 		    done
 		done
 	    done
@@ -63,21 +39,9 @@ then
 	    do
 		for topology in "${VAR_TOPOLOGY[@]}"
 		do
-		    for range in "${VAR_LR_RANGE[@]}"
+		    for check in "${VAR_CHECK_RATE[@]}"
 		    do
-			for alpha in "${VAR_ALPHA[@]}"
-			do
-			    for alpha_div in "${VAR_ALPHA_DIV[@]}"
-			    do
-				for percent in "${VAR_PERCENT[@]}"
-				do
-				    for check in "${VAR_CHECK_RATE[@]}"
-				    do
-					./sr_run.sh $topology $TRAFFIC 0 $arrival $alpha $VAR_STROBE_CNT "${DATE}" $VAR_DATA_ACK $alpha_div 0 $range $check $VAR_ROUTING_NO_ENERGY $seed $MRM $percent
-				    done
-				done
-			    done
-			done
+			./async_run.sh $topology $TRAFFIC 0 $arrival "${LABEL}" $check $seed $TSCH
 		    done
 		done
 	    done
@@ -85,8 +49,8 @@ then
     fi
 fi
 
-# LR_RANGE simulation
-if [ $LR -eq 1 ]
+# TSCH sim
+if [ $TSCH -eq 1 ]
 then
     if [ $TRAFFIC -eq 0 ]
     then
@@ -96,27 +60,9 @@ then
 	    do
 		for topology in "${VAR_TOPOLOGY[@]}"
 		do
-		    for range in "${VAR_LR_RANGE[@]}"
+		    for check in "${VAR_CHECK_RATE[@]}"
 		    do
-			for weight in "${VAR_LR_WEIGHT[@]}"
-			do
-			    for ratio in $VAR_REDUCTION_RATIO
-			    do
-				for alpha in "${VAR_ALPHA[@]}"
-				do
-				    for alpha_div in "${VAR_ALPHA_DIV[@]}"
-				    do
-					for percent in "${VAR_PERCENT[@]}"
-					do
-					    for check in "${VAR_CHECK_RATE[@]}"
-					    do
-						./lr_run.sh $topology $TRAFFIC $period 0 $alpha $VAR_STROBE_CNT $weight $VAR_LSA_R $range $VAR_PARENT_REDUCTION $ratio "${DATE}" $VAR_DATA_ACK $LSA_MAC $alpha_div $check $VAR_LSA_ENHANCED $VAR_ROUTING_NO_ENERGY $ONLY_LONG $seed $MRM $percent
-					    done
-					done
-				    done
-				done
-			    done
-			done
+			./tsch_run.sh $topology $TRAFFIC $period 0 "${LABEL}" $check $seed $TSCH $ORCHESTRA $RBS_SBS $ADAPTIVE_MODE
 		    done
 		done
 	    done
@@ -128,27 +74,9 @@ then
 	    do
 		for topology in "${VAR_TOPOLOGY[@]}"
 		do
-		    for range in "${VAR_LR_RANGE[@]}"
+		    for check in "${VAR_CHECK_RATE[@]}"
 		    do
-			for weight in "${VAR_LR_WEIGHT[@]}"
-			do
-			    for ratio in $VAR_REDUCTION_RATIO
-			    do
-				for alpha in "${VAR_ALPHA[@]}"
-				do
-				    for alpha_div in "${VAR_ALPHA_DIV[@]}"
-				    do
-					for percent in "${VAR_PERCENT[@]}"
-					do
-					    for check in "${VAR_CHECK_RATE[@]}"
-					    do
-						./lr_run.sh $topology $TRAFFIC 0 $arrival $alpha $VAR_STROBE_CNT $weight $VAR_LSA_R $range $VAR_PARENT_REDUCTION $ratio "${DATE}" $VAR_DATA_ACK $LSA_MAC $alpha_div $check $VAR_LSA_ENHANCED $VAR_ROUTING_NO_ENERGY $ONLY_LONG $seed $MRM $percent
-					    done
-					done
-				    done
-				done
-			    done
-			done
+			./tsch_run.sh $topology $TRAFFIC 0 $arrival "${LABEL}" $check $seed $TSCH $ORCHESTRA $RBS_SBS $ADAPTIVE_MODE
 		    done
 		done
 	    done
---
+++ lanada_orchestra/sim_result/tsch_run.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+JOONKI=0
+
+if [ $JOONKI -eq 0 ]
+then
+    CONTIKI=/media/user/Harddisk/contiki-orchestra/
+else
+    CONTIKI=~/Desktop/contiki-orchestra/
+fi
+
+echo "TSCH simulation"
+#sed -i 's/\#define DUAL_RADIO 0/\#define DUAL_RADIO 1/g' $CONTIKI/platform/cooja/contiki-conf.h
+sed -i 's/\#define TCPIP_CONF_ANNOTATE_TRANSMISSIONS 1/\#define TCPIP_CONF_ANNOTATE_TRANSMISSIONS 0/g' $CONTIKI/platform/cooja/contiki-conf.h
+
+topology=$1
+TRAFFIC_MODEL=$2
+PERIOD=$3
+ARRIVAL_RATE=$4
+LABEL=$5
+CHECK=$6
+SEED_NUMBER=$7
+TSCH=$8
+ORCHESTRA=$9
+RBS_SBS=${10}
+ADAPTIVE=${11}
+
+sed -i "11s/.*/    <randomseed>$SEED_NUMBER<\/randomseed>/" $CONTIKI/lanada_orchestra/sim_script/$topology\.csc 
+
+if [ $TRAFFIC_MODEL -eq 0 ]
+then
+    DIR=$LABEL\_TSCH$TSCH\_topo$topology\_traffic$TRAFFIC_MODEL\_period$PERIOD\_seed$SEED_NUMBER
+else
+    DIR=$LABEL\_TSCH$TSCH\_topo$topology\_traffic$TRAFFIC_MODEL\_rate$ARRIVAL_RATE\_seed$SEED_NUMBER
+fi
+
+mkdir $DIR
+cd $DIR
+
+../param.sh 
+
+IN_DIR=check$CHECK
+if [ ! -e $IN_DIR ]
+then
+    mkdir $IN_DIR
+#    mkdir sr\_strobe$STROBE_CNT\_$LR_range\_$CHECK\_rou$ROUTING_NO_ENERGY
+fi
+cd $IN_DIR
+#cd sr\_strobe$STROBE_CNT\_$LR_range\_$CHECK\_rou$ROUTING_NO_ENERGY
+echo "#########################  We are in $PWD  ########################"
+
+HERE=$PWD
+cd $CONTIKI/lanada_orchestra
+make clean TARGET=cooja
+cd $HERE
+
+if [ ! -e COOJA.testlog ]
+then
+    java -mx512m -jar $CONTIKI/tools/cooja/dist/cooja.jar -nogui=$CONTIKI/lanada_orchestra/sim_script/$topology\.csc -contiki="$CONTIKI"
+	#	java -mx512m -classpath $CONTIKI/tools/cooja/apps/mrm/lib/mrm.jar: -jar $CONTIKI/tools/cooja/dist/cooja.jar -nogui=$CONTIKI/lanada/sim_scripts/scripts/0729_$topology\_$LR_range\.csc -contiki="$CONTIKI"
+		# ant run_nogui -Dargs=/home/user/Desktop/Double-MAC/lanada/sim_scripts/scripts/0729_36grid_2X.csc -Ddir=$PWD
+	#	ant run_nogui -Dargs=/home/user/Desktop/Double-MAC/lanada/sim_scripts/scripts/0729_36grid_2X.csc
+fi
+# else # If MRM mode
+# 	if [ ! -e COOJA.testlog ]
+# 	then
+# 		cd $CONTIKI/tools/cooja_mrm$MRM 
+# 		if [ $ONLY_LONG -eq 0 ]
+# 		then
+# 			ant run_nogui -Dargs=$CONTIKI/lanada/sim_scripts/scripts/$topology\_$LR_range\.csc
+# 		else
+# 			ant run_nogui -Dargs=$CONTIKI/lanada/sim_scripts/scripts/$topology\_$LR_range\_L\.csc
+# 		fi
+# 		mv build/COOJA.testlog $HERE
+# 		cd $HERE
+# 	fi
+# fi
+
+
+if [ ! -e report_summary.txt ]
+then
+    ../../pp.sh
+fi
+cd ../..
+
+echo "Simulation finished"
