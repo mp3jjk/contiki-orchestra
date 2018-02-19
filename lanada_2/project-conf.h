568ede30c1a7a873311a4ed561e3a98df4b97ecc
mp3jjk <jhjung@lanada.kaist.ac.kr>
2018-02-19 21:19:54 +0900

    tree_c4_21 added

--- lanada_2/project-conf.h
+++ lanada_2/project-conf.h
@@ -26,7 +26,7 @@
 
 #define TRAFFIC_PATTERN 0	// 0: Periodic, 1: Event-driven /* jk */
 #if TRAFFIC_PATTERN == 0 // If periodic
-#define PERIOD	10 /* jk */
+#define PERIOD	30 /* jk */
 #else	// If event driven (assume poisson)
 #define INTENSITY 0 // lambda /* jk */
 #endif
