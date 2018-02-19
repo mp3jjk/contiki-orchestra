1a2a1bf6ab3b1c803af9f5382c1c3392d281dcad
Joonki <jkhong12000@gmail.com>
2018-02-19 22:51:36 +0900

    minor upgrade

--- lanada_orchestra/sim_result/peek.sh
+++ lanada_orchestra/sim_result/peek.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 for dir in *; do
-    echo $dir
+    echo "############# $dir ############"
     cd $dir
     echo "duty cycle"
     tail -n 4 parsing/AVG.txt
 
     echo "delay"
-    cat delay/avg_packet_delay.txt
+		cat delay/avg_packet_delay.txt
+		sum_delay=`awk -F ' ' '{sum += $3} END {print sum}' delay/avg_packet_delay.txt`
+		num_delay=`wc -l < "delay/avg_packet_delay.txt"` 
+		echo "avr_delay"
+		echo "scale=1;$sum_delay/$num_delay"|bc
+		echo " us"
+
 
     echo "PRR"
     cat PRR/PRR.txt
+		sum_PRR=`awk -F ' ' '{sum += $2} END {print sum}' PRR/PRR.txt`
+		num_PRR=`wc -l < "PRR/PRR.txt"`
+		echo "avr_PRR"
+		echo "scale=3;$sum_PRR/$num_PRR"|bc
+		echo " %"
+
+
     cd ..
 done
