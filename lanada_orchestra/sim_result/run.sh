9e5d3f8797b68ff1db0a85404506ef5ffc72956f
mp3jjk <jhjung@lanada.kaist.ac.kr>
2019-07-20 16:33:29 +0200

    modified run.sh

--- lanada_orchestra/sim_result/run.sh
+++ lanada_orchestra/sim_result/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-label=PAAS3AX #**
+label=jul20 #**
 
 tsch=1 # Whether Async(0) or TSCH(1)
 orchestra=1 # Whether Minimal(0) or Orchestra(1) **
@@ -8,21 +8,20 @@ sbs=1 # Whether RBS(0) or SBS(1) **
 paas=1 # Whether basic(0) or PAAS(1) **
 hetero=0
 poisson=1 # Whether Periodic(0) or Poisson(1) **
-req=95 # reliability contraint
-sim_time=36000000 #**
 avoid=1 # Avoid the situation that node id is multiple of slotframe length
 
 CHECK_RATE=( 8 )
 
-SEED=( 1 2 ) #**
-TRAFFIC_PARAM=( 20 ) # rate or period
-TOPOLOGY=( random_40 ) # tree_c4_21 grid_36 random_40 child_4 **
+SEED=( 1 2 3 4 5 6 ) #**
+TRAFFIC_PARAM=( 10 20 40 80 ) # rate or period
+TOPOLOGY=( random_40 tree_c4_21 ) # tree_c4_21 grid_36 random_40 child_4 **
 SCHED_PARAM=( 0 ) # n-pbs(paas) or n-sf(ours)
 SF_LENGTH=( 19 ) # SlotFrame length for Orchestra and paas **
 SF_EB=( 20 ) # How many times the length of EB slotframe is the length of unicast slotframe.
 SF_COMMON=( 2 ) # How many times the length of EB slotframe is the length of unicast slotframe.
 MAX_RT=( 3 ) # maximum retries
 STATIC=( 0 )
+REQ=( 95 98 ) # reliability contraint
 
 app=${1:-1} # Defualt 1
 replace=${2:-0} # Default 0.
@@ -34,7 +33,10 @@ if [ $tsch -eq 0 ]; then
 		for traffic_param in "${TRAFFIC_PARAM[@]}"; do
 			for topology in "${TOPOLOGY[@]}";	do
 				for check_rate in "${CHECK_RATE[@]}";	do
-					./async_run.sh $topology $poisson $traffic_param $label $check_rate $seed $tsch $app $sim_time $replace
+					for req in "${REQ[@]}"; do
+						sim_time=$((traffic_param*1500000))
+						./async_run.sh $topology $poisson $traffic_param $label $check_rate $seed $tsch $app $sim_time $replace
+					done
 				done
 			done
 		done
@@ -49,10 +51,13 @@ else
 							for sf_eb in "${SF_EB[@]}"; do
 								for sf_common in "${SF_COMMON[@]}"; do
 									for static in "${STATIC[@]}"; do
-										./tsch_run.sh $label $orchestra $sbs $paas $hetero \
-																	$poisson $req $sim_time $seed $traffic_param \
-																	$topology $sched_param $sf_length $max_rt $app \
-																	$avoid $sf_eb $sf_common $replace $static 
+										for req in "${REQ[@]}"; do
+											sim_time=$((traffic_param*1500000))
+											./tsch_run.sh $label $orchestra $sbs $paas $hetero \
+																		$poisson $req $sim_time $seed $traffic_param \
+																		$topology $sched_param $sf_length $max_rt $app \
+																		$avoid $sf_eb $sf_common $replace $static 
+										done
 									done
 								done
 							done
