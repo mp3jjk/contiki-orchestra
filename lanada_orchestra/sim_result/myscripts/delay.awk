2adcbcb96873267f2f22d10057cb7705dd840309
mp3jjk <jhjung@lanada.kaist.ac.kr>
2019-07-23 17:29:03 +0200

    shells

---
+++ lanada_orchestra/sim_result/myscripts/delay.awk
@@ -0,0 +1,33 @@
+# delay.awk
+# Input: COOJA.testlog
+# field: time/(int)/app//DATA/id/(int)/from/(int)/ASN/(int) : 11 fields
+# field: time/1/DATA/recv/'DATA/id/(int)/from/(int)' : 9 fields
+# Use: awk -f delay.awk num_node=40 (or 21)
+
+BEGIN { 
+	FS="[: ]"
+	for(node_id=1; node_id<=num_node; node_id++) 
+		count[node_id]=0
+}
+( $3 ~ /app/ ) && ( $5 ~ /DATA/ ) { 
+	node_id=int($9)
+	data_id[node_id]=$7
+	time_send[node_id]=$1 
+}
+( $3 ~ /DATA/ ) && ( $4 ~ /recv/ ) { 
+	node_id=int($9) # int() can remove " ' "
+	if (data_id[node_id] == int($7)) {
+		time_recv[node_id]=$1
+		delay[node_id]=time_recv[node_id]-time_send[node_id]
+		sum_delay[node_id]+=delay[node_id]
+		count[node_id]++
+	}
+}
+END {
+	print count[1]
+	for(node_id=1; node_id<=num_node; node_id++) {
+		if (count[node_id]==0) printf("%d ", 0)
+		else printf("%10d", sum_delay[node_id]/count[node_id])
+	}
+	printf("\n")
+}
---
+++ lanada_orchestra/sim_result/myscripts/movedir.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+. var.sh
+
+label=$1 
+SEED=(1 2 3 4 5 6 7 8)
+
+for req in "${REQ[@]}"; do
+	for traffic_param in "${TRAFFIC_PARAM[@]}";	do
+		for topology in "${TOPOLOGY[@]}";	do
+			for sched_param in "${SCHED_PARAM[@]}";	do
+				for sf_length in "${SF_LENGTH[@]}";	do
+					for max_rt in "${MAX_RT[@]}";	do
+						for sf_eb in "${SF_EB[@]}"; do
+							for sf_common in "${SF_COMMON[@]}"; do
+								for static in "${STATIC[@]}"; do
+									for metric in "${METRIC[@]}"; do
+										for seed in "${SEED[@]}"; do
+											# Open directories
+											DIR=${label}_${topology}_${HETERO_STRING[$hetero]}_${POISSON_STRING[$poisson]}${traffic_param}
+											if [ $orchestra -eq 1 ]; then
+												if [ $paas -eq 1 ]; then # paas
+													IN_DIR=paas_n_pbs${sched_param}_sf_length${sf_length}_sf_eb${sf_eb}_sf_common${sf_common}_static${static}_avoid${avoid}_max_rt${max_rt}_req${req}
+												else # orchestra
+													IN_DIR=orchestra_${SBS_STRING[${sbs}]}_sf_length${sf_length}_sf_eb${sf_eb}_sf_common${sf_common}_static${static}_avoid${avoid}_max_rt${max_rt}_req${req}
+												fi
+											else #minimal
+												IN_DIR=minimal_sf_length${sf_length}_sf_eb${sf_eb}_sf_common${sf_common}_static${static}_avoid${avoid}_max_rt${max_rt}_req${req}
+											fi
+											dir=${DIR}_${seed}/${IN_DIR}
+											dir2=${DIR}_${IN_DIR}
+											# Check whether logfile exists
+											echo $dir
+											if [ -d ${dir} ]; then
+												print 1
+												cp ${dir}/COOJA.testlog log_${dir2}_${seed}
+											fi
+										done
+									done
+								done
+							done
+						done
+					done
+				done
+			done
+		done
+	done
+done
