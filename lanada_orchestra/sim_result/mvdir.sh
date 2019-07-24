43baaf9a865d4e35f4ca6bf33b106196eb4047e2
mp3jjk <jhjung@lanada.kaist.ac.kr>
2019-07-24 16:09:12 +0200

    compatiable with old directory structure

--- lanada_orchestra/sim_result/mvdir.sh
+++ lanada_orchestra/sim_result/mvdir.sh
@@ -1,12 +1,17 @@
 #!/bin/bash 
 
 label=$1 
+old=$2
 
 for DIR in ${1}*; do
 	cd $DIR
 	for IN_DIR in *; do
-		seed=$(echo $DIR | awk '{ print substr($1, length($1)) }')
-		FILE_F=$(echo $DIR | awk '{print substr($1, 1, length($1)-1)}' )
+		if [ $old -e 1 ]; then
+			FILE_F=$(echo $DIR | awk -F'_seed' '{print $1}')
+		else
+			seed=$(echo $DIR | awk '{ print substr($1, length($1)) }')
+			FILE_F=$(echo $DIR | awk '{print substr($1, 1, length($1)-1)}' )
+		fi
 		FILE=log_${FILE_F}${IN_DIR}_seed${seed}
 		cp ${IN_DIR}/COOJA.testlog ../${FILE}
 	done
