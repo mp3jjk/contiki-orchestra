ec9fe4dd296cb638d0394b64d49abdf639b5f051
mp3jjk <jhjung@lanada.kaist.ac.kr>
2019-07-25 12:04:04 +0200

    script update

--- lanada_orchestra/sim_result/compare.sh
+++ lanada_orchestra/sim_result/compare.sh
@@ -12,7 +12,7 @@ for metric in "${METRIC[@]}"; do
 done
 
 for metric in "${METRIC[@]}"; do
-	for FILE in ${metric}_*; do
+	for FILE in allseed_${metric}_*; do
 		COMMON=$(echo $FILE | awk -F"_$compare" '{ print $1substr($2,index($2,"_")+1) }')
 		diff=$(echo $FILE | awk -F"_$compare" '{ print substr($2,1,index($2,"_")-1) }')
 		echo "$diff  |   $(awk -f avr.awk num_node=${num_node} ${FILE})" >> compare_${COMMON}
--- lanada_orchestra/sim_result/parsing_v2.sh
+++ lanada_orchestra/sim_result/parsing_v2.sh
@@ -13,7 +13,7 @@ for metric in "${METRIC[@]}"; do
 done
 
 for metric in "${METRIC[@]}"; do
-	for FILE in log_${1}*; do
+	for FILE in ${1}*; do
 		#seed=$(echo $FILE | awk -F'seed' '{print $2}')
 		NAME=$(echo "$FILE" | awk -F'_seed' '{print $1}')
 		awk -f ${metric}.awk num_node=${num_node} ${FILE} >> allseed_${metric}_${NAME}
