d7e820e77dd3970e4efbfa640866d81bc0e6f6bb
mp3jjk <jhjung@lanada.kaist.ac.kr>
2019-10-05 16:49:33 +0200

    scp fix

--- lanada_orchestra/sim_result/scp.sh
+++ lanada_orchestra/sim_result/scp.sh
@@ -4,6 +4,6 @@ TARGET=$1
 
 tar -zcvf ${TARGET}.tar.gz allseed_*
 
-scp ${TARGET}.tar.gz jinhwanjung@143.248.57.148:~/tpsn_result/
+scp ${TARGET}.tar.gz jinhwanjung@143.248.57.148:~/ipsn_result/
 
 # scp allseed_* jhjung@cafe3.kaist.ac.kr:~/infocom_result/${TARGET}
